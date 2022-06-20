5cd63d428d622e0973df39015946ee04b1e03b9e
Artemii Gazizianov <Artemii.Gazizianov@gmail.com>
2022-06-20 20:49:29 +0400

    Functional Components

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,77 +1,76 @@
 import React, { Component } from "react";
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class Dishdetail extends Component {
-    constructor(props) {
-        super(props);
-    }
 
-    renderDish(dish) {
-        if (dish != null) {
-            return (
+
+function RenderDish({ dish }) {
+    if (dish != null) {
+        return (
+            <div className="col-12 col-md-5 m-1">
                 <Card>
                     <CardImg width="100" src={dish.image} alt={dish.name} />
                     <CardBody>
-                        <CardTitle heading>{dish.name}</CardTitle>
+                        <CardTitle>{dish.name}</CardTitle>
                         <CardText>
                             {dish.description}
                         </CardText>
                     </CardBody>
                 </Card>
-            );
-        }
-        else {
-            return (
-                <div></div>
-            );
-        }
+            </div>
+        );
     }
-
-    renderComments(comments) {
-        if (comments != null) {
-            return comments.map((comment) => {
-                return (
-                    <li key={comment.id} className="list-unstyled">
-                        <p>{comment.comment}</p>
-                        <p>-- {comment.author}, &nbsp;
-                            {new Intl.DateTimeFormat("en-US", {
-                                year: "numeric",
-                                month: "long",
-                                day: "2-digit",
-                            }).format(new Date(comment.date))}</p>
-                    </li>
-                );
-            });
-        }
-        else {
-            return (
-                <div></div>
-            );
-        }
-    }
-
-    render() {
-        const selectedDish = this.props.selectedDish;
-
-        if (selectedDish != null) {
-            return (
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderDish(selectedDish)}
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <h4>Comments</h4>
-                        {this.renderComments(selectedDish.comments)}
-                    </div>
-                </div>
-            );
-        }
-        else {
-            return (
-                <div></div>
-            );
-        }
+    else {
+        return (
+            <div></div>
+        );
     }
 }
+
+function RenderComments({ comments }) {
+    if (comments != null) {
+        const renderComments = comments.map((comment) => {
+            return (
+                <li key={comment.id} className="list-unstyled">
+                    <p>{comment.comment}</p>
+                    <p>-- {comment.author}, &nbsp;
+                        {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
+                </li>
+            );
+        });
+
+        return (
+            <div className="col-12 col-md-5 m-1">
+                <h4>Comments</h4>
+                {renderComments}
+            </div>
+        );
+    }
+    else {
+        return (
+            <div></div>
+        );
+    }
+}
+
+const Dishdetail = (props) => {
+    const selectedDish = props.dish;
+
+    if (selectedDish != null) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <RenderDish dish={props.dish} />
+                    <RenderComments comments={selectedDish.comments} />
+                </div>
+            </div>
+        );
+    }
+    else {
+        return (
+            <div></div>
+        );
+    }
+}
+
 
 export default Dishdetail;
