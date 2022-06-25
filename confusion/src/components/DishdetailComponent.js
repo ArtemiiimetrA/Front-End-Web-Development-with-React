edc3922cfcd6450f6a390743fcf26991f0e62c17
Artemii Gazizianov <Artemii.Gazizianov@gmail.com>
2022-06-25 14:44:23 +0400

    Redux Thunk

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -8,6 +8,7 @@ import {
 } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 
 function RenderDish({ dish }) {
@@ -157,9 +158,25 @@ class CommentForm extends Component {
 }
 
 const Dishdetail = (props) => {
-    const selectedDish = props.dish;
-
-    if (selectedDish != null) {
+    if(props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if(props.errMess){
+        return (
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null) {
         return (
             <div className="container">
                 <div className="row">
